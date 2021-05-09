45fcfc1bf99cee83a3aa2ec3cad9b641f5c9bf30
bakhtiarabid <abid.bu16@gmail.com>
2021-05-09 14:26:48 +0600

    BMI Calculator

--- calculator.js
+++ calculator.js
@@ -10,12 +10,25 @@ app.get("/", function(req, res){
    res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmicalculator", function (req, res) {
+   res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.post("/", function(req, res) {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
 
     var result = num1 + num2;
    res.send(" Congratulations!!! you have just made it " + result);
+});
+
+app.post("/bmicalculator", function (req, res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+   var cal = Math.round(num1 / Math.pow(num2, 2));
+
+   var result = cal;
+   res.send("<em><strong> Your BMI is</strong></em> " + result);
 });
 
 app.listen(port, function()  {
