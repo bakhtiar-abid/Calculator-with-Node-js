46ed87b4e23967773455933a1bb424009036710d
bakhtiarabid <abid.bu16@gmail.com>
2021-05-09 13:06:27 +0600

    Calculator

--- calculator.js
+++ calculator.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-   res.send("Hello World!");
+   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+   res.send("Congrations!!! you have just posted it Great Day");
 });
 
 app.listen(port, () => {
